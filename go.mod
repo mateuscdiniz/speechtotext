7f594378c9f15966fed10ad4511e883ab0ada751
mateus <mateuscdiniz@gmail.comgit config --global user.email mateuscdiniz@gmail.com>
2020-05-19 16:54:57 -0300

    creating crud

---
+++ go.mod
@@ -0,0 +1,10 @@
+module api-test
+
+go 1.13
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/gorilla/mux v1.7.4
+	github.com/jinzhu/gorm v1.9.12
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+)
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Article struct {
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	var article Article
+
+	json.Unmarshal(reqBody, &article)
+
+	//atualizando o array com o novo
+	//artigo
+	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	// once again, we will need to parse the path parameters
+	vars := mux.Vars(r)
+	// we will need to extract the `id` of the article we
+	// wish to delete
+	id := vars["id"]
+
+	// we then need to loop through all our articles
+	for index, article := range Articles {
+		// if our id path parameter matches one of our
+		// articles
+		if article.Id == id {
+			// updates our Articles array to remove the
+			// article
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+
+}
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Endpoint Hit: All articles Endpoint")
+
+	//codifica o array de artigos em string json
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage endpoint Hit")
+}
+
+//função que mapeia as chamadas
+func handleRequests() {
+
+	//criando um novo roteador
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", homePage)
+	router.HandleFunc("/article", createNewArticle).Methods("POST")
+	router.HandleFunc("/articles", allArticles)
+	router.HandleFunc("/article/{id}", returnSingleArticle)
+	router.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+
+}
+
+func main() {
+
+	Articles = []Article{
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+
+	handleRequests()
+}
